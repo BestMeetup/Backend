05ea6fb5afee5d674d8927f01ebb4fd45b9977f1
Rodrigo Dias <rodrigogmdias@gmail.com>
2017-06-22 15:52:48 -0300

    Change ip deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,11 +3,11 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "138.197.86.190", user: "root", roles: %w{app db web}
+server "bestmeetup.com.br", user: "root", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :nginx_server_name, "localhost bestmeetup.com.br www.bestmeetup.com.br"
 
 # role-based syntax
 # ==================
