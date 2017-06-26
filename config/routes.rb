e6c650ecb04372dc287d29203c7970a0f53a59ed
Rodrigo Dias <rodrigogmdias@gmail.com>
2017-06-26 12:53:18 -0300

    resolve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'v1'
+  mount_devise_token_auth_for 'User', at: 'v1/auth'
   
   devise_for :admins
-  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
