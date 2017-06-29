b40281722cfddd1c2a8fb17e27db9789daba104c
rodrigodias-us <contato@rodrigodias.us>
2017-06-29 21:43:34 +0000

    Generated code by SwaggerHub

--- public/swagger/javascript/src/api/UsuarioApi.js
+++ public/swagger/javascript/src/api/UsuarioApi.js
@@ -25,18 +25,18 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/DeviseResponseUser', 'model/User'], factory);
+    define(['ApiClient', 'model/DeviseResponseError', 'model/DeviseResponseUser', 'model/User'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(require('../ApiClient'), require('../model/DeviseResponseUser'), require('../model/User'));
+    module.exports = factory(require('../ApiClient'), require('../model/DeviseResponseError'), require('../model/DeviseResponseUser'), require('../model/User'));
   } else {
     // Browser globals (root is window)
     if (!root.SwaggerBestMeetup) {
       root.SwaggerBestMeetup = {};
     }
-    root.SwaggerBestMeetup.UsuarioApi = factory(root.SwaggerBestMeetup.ApiClient, root.SwaggerBestMeetup.DeviseResponseUser, root.SwaggerBestMeetup.User);
+    root.SwaggerBestMeetup.UsuarioApi = factory(root.SwaggerBestMeetup.ApiClient, root.SwaggerBestMeetup.DeviseResponseError, root.SwaggerBestMeetup.DeviseResponseUser, root.SwaggerBestMeetup.User);
   }
-}(this, function(ApiClient, DeviseResponseUser, User) {
+}(this, function(ApiClient, DeviseResponseError, DeviseResponseUser, User) {
   'use strict';
 
   /**
