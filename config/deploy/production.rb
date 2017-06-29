3e2761324fb1cfc0a74678673b2058e37f328ae4
Rodrigo Dias <rodrigogmdias@gmail.com>
2017-06-29 15:33:51 -0300

    Update Travis and Capstrano Login for password

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "bestmeetup.com.br", user: "root", roles: %w{app db web}
+server "bestmeetup.com.br", user: ENV['BESTMEETUP_USER'], password: ENV['BESTMEETUP_PASSWORD'], roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
